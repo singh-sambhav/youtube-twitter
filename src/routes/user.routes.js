4388a608f742ad97cf86a75d2574118896d79947
sambhav <sambhav962@gmail.com>
2024-02-07 01:50:40 +0530

    Add user account management endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser , logoutUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser , logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -23,4 +23,5 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJWT , logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
